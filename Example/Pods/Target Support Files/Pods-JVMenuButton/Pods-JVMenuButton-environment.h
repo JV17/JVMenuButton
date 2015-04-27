6a3069e6973996d0420b915d4501c94b548f0c61
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-04-27 00:19:27 -0400

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-JVMenuButton/Pods-JVMenuButton-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// JVMenuButton
+#define COCOAPODS_POD_AVAILABLE_JVMenuButton
+#define COCOAPODS_VERSION_MAJOR_JVMenuButton 0
+#define COCOAPODS_VERSION_MINOR_JVMenuButton 1
+#define COCOAPODS_VERSION_PATCH_JVMenuButton 0
+
